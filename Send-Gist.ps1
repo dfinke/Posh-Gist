b4496896df57387a842bf26942329f3b83cd8cc6
Doug Finke <finked@hotmail.com>
2013-12-11 10:54:59 -0500

    Open gist url after posting

--- Send-Gist.ps1
+++ Send-Gist.ps1
@@ -15,5 +15,6 @@ function Send-Gist {
 
     $Header = Get-GistAuthHeader
     $resp   = Invoke-RestMethod -Uri 'https://api.github.com/gists' -Method Post -Headers $Header -Body ($gist | ConvertTo-Json)
-    "{0} posted {1}" -f $fileName, $resp.'html_url'
+
+    Start-Process $resp.'html_url'
 }
